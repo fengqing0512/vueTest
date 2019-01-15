9b9c3b6d169109372f497cecacfcd9e371e7e65f
fengqing <909051267@qq.com>
2019-01-15 21:12:36 +0800

    add project mall

---
+++ src/view/test.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="test">
+		this is id : {{$route.params.testId}}
+	</div>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				msg: 'hello vue'
+			}
+		}
+	}
+</script>
+<style scoped></style>
